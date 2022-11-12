49118f0d004e78c34d96b3899c0383e09da80e84
IanaCris <ianasousati@gmail.com>
2022-11-11 23:55:52 -0300

    Use prisma client

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,22 @@
 import Fastify from "fastify";
+import cors from "@fastify/cors";
+
+import { groupRoutes } from "./routes/groups";
 
 async function bootstrap() {
     const fastify = Fastify({
         logger: true,
     });
 
-    fastify.get("/polls/count", () => {
-        return { count: 0 }
+    await fastify.register(cors, {
+        origin: true,
     });
 
+    await fastify.register(groupRoutes);
+
+
     await fastify.listen({ port: 3333 });
+    // await fastify.listen({ port: 3333, host: '0.0.0.0' })
 }
 
 bootstrap();
